fcc5e228e2370d05ee6e3625041e9c956ec7068d
Jussivan <jussivanbm1029@gmail.com>
2025-03-05 13:56:39 -0300

    create Projects Routes

--- src/routes/projectRoutes/projectRoutes.js
+++ src/routes/projectRoutes/projectRoutes.js
@@ -2,22 +2,22 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function certificateRoutes(app) {
+export async function projectRoutes(app) {
     app.post('/', async (request, reply) => {
         try {
-            let { name, description, imageUrl, certificateUrl, tags, createdAt } = request.body;
+            let { name, description, imageUrl, projectUrl, tags, createdAt } = request.body;
             createdAt = new Date(createdAt).toISOString();
-            await prisma.certificate.create({
+            await prisma.project.create({
                 data: {
                     name,
                     description,
                     imageUrl,
-                    certificateUrl,
+                    projectUrl,
                     tags,
                     createdAt
                 }
             });
-            return reply.status(201).send('Certificado criado com sucesso!');
+            return reply.status(201).send('Projeto criado com sucesso!');
         } catch(err) {
             return reply.status(409).error(err);
         }
@@ -25,23 +25,23 @@ export async function certificateRoutes(app) {
 
     app.get('/', async (request,reply) => {
         try {
-            let certificates = await prisma.certificate.findMany();
+            let projects = await prisma.project.findMany();
             return reply.status(200).send({
-                message: 'Certificados listados com sucesso!',
-                data: certificates
+                message: 'Projetos listados com sucesso!',
+                data: projects
             });
         } catch(err) {
             return reply.status(409).error(err);
         }
     });
 
-    app.post('/findCertificate', async (request,reply) => {
+    app.post('/findProject', async (request,reply) => {
         try {
             let { id } = request.body;
-            let certificate = await prisma.certificate.findMany({where: {id : id}});
+            let project = await prisma.project.findMany({where: {id : id}});
             return reply.status(200).send({
-                message: 'Certificados listado com sucesso!',
-                data: certificate
+                message: 'Projeto listado com sucesso!',
+                data: project
             });
         } catch(err) {
             return reply.status(409).console.error(err);
@@ -50,21 +50,21 @@ export async function certificateRoutes(app) {
 
     app.put('/', async (request,reply) => {
         try {
-            let { id, name, description, imageUrl, certificateUrl, tags, createdAt } = request.body;
-            const certificate = await prisma.certificate.update({
+            let { id, name, description, imageUrl, projectUrl, tags, createdAt } = request.body;
+            const project = await prisma.project.update({
                 where: { id: id },
                 data: {
                     name,
                     description,
                     imageUrl,
-                    certificateUrl,
+                    projectUrl,
                     tags,
                     createdAt: new Date(createdAt).toISOString()
                 }
             });
             return reply.status(200).send({
-                message: 'Certificados atualizado com sucesso!',
-                data: certificate
+                message: 'Projeto atualizado com sucesso!',
+                data: project
             });
         } catch(err) {
             return reply.status(409).console.error(err);
@@ -74,8 +74,8 @@ export async function certificateRoutes(app) {
     app.delete('/', async (request,reply) => {
         try {
             const { id } = request.body;
-            await prisma.certificate.delete({where: {id : id}});
-            return reply.status(200).send('Certificado excluído com sucesso!');
+            await prisma.project.delete({where: {id : id}});
+            return reply.status(200).send('Projeto excluído com sucesso!');
         } catch(err) {
             return reply.status(209).console.error(err);
         }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import fastify from "fastify";
 import { certificateRoutes } from "./routes/certificateRoutes/certificateRoutes.js";
+import { projectRoutes } from "./routes/projectRoutes/projectRoutes.js";
 
 const app = fastify();
 
 app.register(certificateRoutes, {
     prefix: 'certificates',
-})
-
+});
+app.register(projectRoutes, {
+    prefix: 'projects',
+});
 
 app.listen({ port: 3000 }).then(() => {
     console.log('Servidor rodando na porta 3000');
